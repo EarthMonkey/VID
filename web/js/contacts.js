a80e2e08be1cdb5147b919d60c4037e4e38d778e
SURE <794637366@qq.com>
2017-01-11 19:11:52 +0800

    联系人页面

--- web/js/contacts.js
+++ web/js/contacts.js
@@ -2,8 +2,11 @@
  * Created by L.H.S on 2017/1/10.
  */
 
+var last_contact_click = null;
+
 window.onload = function () {
     addIndex();
+    initContacts();
 };
 
 function search() {
@@ -27,14 +30,56 @@ function addIndex() {
     var parent = document.getElementById("indexA");
 
     var a = 65;
-    for (var i=0; i<26; i++) {
+    for (var i = 0; i < 26; i++) {
         var div = document.createElement("div");
-        div.innerHTML = String.fromCharCode(a+i);
+        div.innerHTML = String.fromCharCode(a + i);
         parent.appendChild(div);
+
+        $(div).click(function () {
+            var pos = $("#" + this.innerHTML).offset().top;
+            $(".contacts_list").animate({scrollTop: pos}, 100);
+        });
     }
 
-    var div = document.createElement("div");
-    div.innerHTML = "#";
-    parent.appendChild(div);
+    var diva = document.createElement("div");
+    diva.innerHTML = "#";
+    parent.appendChild(diva);
+
+    $(".contacts_list").on('scroll', function () {
+        parent.style.top = 90 + $(".contacts_list").scrollTop() + "px";
+    });
+}
+
+function initContacts() {
+
+    var parent = document.getElementById("lists");
+
+    var a = 65;
+    for (var i = 0; i < 3; i++) {
+        var idx = document.createElement("div");
+        idx.className = "each_idx";
+        var idx_name = String.fromCharCode(a + i);
+        idx.innerHTML = idx_name;
+        $(idx).attr("id", idx_name);
+        parent.appendChild(idx);
+
+        for (var j = 1; j < 6; j++) {
+            var con_div = document.createElement("div");
+            con_div.className = "each_contact";
+            con_div.innerHTML = idx_name + "姓联系人" + j + "<hr>"
+            parent.appendChild(con_div);
+
+            $(con_div).click(function () {
+
+                if (last_contact_click != null)
+                    $(last_contact_click).css("background-color", "transparent");
+
+                $(this).css("background-color", "#d9eef9");
+                last_contact_click = this;
+            });
+
+        }
+
+    }
 
 }
