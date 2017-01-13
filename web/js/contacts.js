c69d19b6b29dee5e49e4d1edd75234a3912f086c
SURE <794637366@qq.com>
2017-01-13 20:30:23 +0800

    联系人页面&我的

--- web/js/contacts.js
+++ web/js/contacts.js
@@ -7,6 +7,7 @@ var last_contact_click = null;
 window.onload = function () {
     addIndex();
     initContacts();
+    getVideos();
 };
 
 function search() {
@@ -16,6 +17,8 @@ function search() {
         $("#search_cancel").click(function () {
             $("#search").val("");
             $(this).hide();
+            $("#search_list").hide();
+            $("#lists").show();
         });
     } else {
         if ($("#search").val() == "") {
@@ -23,7 +26,7 @@ function search() {
         }
     }
 
-    if($("#lists").css("display") != "none" && $("#search").val() != "") {
+    if ($("#lists").css("display") != "none" && $("#search").val() != "") {
         $("#lists").hide();
         $("#search_list").show();
     } else {
@@ -92,6 +95,7 @@ function initContacts() {
     }
 }
 
+// 视频放大
 function enlarge(node) {
 
     var vid = node.parentNode.getElementsByTagName("video")[0];
@@ -116,4 +120,103 @@ function enlarge(node) {
     $(video_copy).css("width", "100%");
     $(video_copy).css("margin", "30px auto");
 
+}
+
+// 滑出我的名片夹
+function showMine() {
+
+    $("#detail_part").hide();
+
+    initMyVideos();
+    $("#mine").animate({
+        width: "show"
+    }, 200);
+
+    $(".group_lbl, .contacts_list").click(function (e) {
+        var elem = e.target || e.srcElement;
+        if (elem.className && elem.className == "each_group first_bottom") {
+            return;
+        }
+        hideMine();
+    });
+}
+
+// 点击侧边滑回
+function hideMine() {
+    if ($("#mine").css("display") != "none") {
+        $("#mine").animate({
+            width: "hide"
+        }, 200);
+        $("#detail_part").show();
+    }
+
+    if ($("#left_part").css("display") != "none") {
+        $("#left_part").animate({
+            width: "hide"
+        }, 200);
+    }
+
+    var mine = document.getElementById("mine");
+    var tips = mine.getElementsByClassName("tip_text");
+    $(tips[0]).show();
+    $(tips[1]).hide();
+}
+
+// 我的视频
+function initMyVideos() {
+
+    var parent = document.getElementById("my_videos");
+    var copy = document.getElementById("video_copy");
+
+    var number = 3;
+    $("#my_videos").css("width", 230 * number + "px");
+    for (var i = 0; i < number; i++) {
+
+        var div = document.createElement("div");
+        div.className = "video_div";
+        div.innerHTML = copy.innerHTML;
+
+        var source = div.getElementsByTagName("source")[0];
+        var name_div = div.getElementsByClassName("video_name")[0];
+
+        source.src = "https://media.html5media.info/video.iphone.mp4";
+        name_div.innerHTML = "我的视频" + (i + 1);
+
+        parent.appendChild(div);
+    }
+}
+
+// 联系人的视频
+function getVideos() {
+
+    var parent = document.getElementById("videos");
+    var copy = document.getElementById("video_copy");
+
+    var number = 3;
+    for (var i = 0; i < number; i++) {
+
+        var div = document.createElement("div");
+        div.className = "video_div";
+        div.innerHTML = copy.innerHTML;
+
+        var source = div.getElementsByTagName("source")[0];
+        var name_div = div.getElementsByClassName("video_name")[0];
+
+        source.src = "https://media.html5media.info/video.iphone.mp4";
+        name_div.innerHTML = "我的视频" + (i + 1);
+
+        parent.appendChild(div);
+    }
+}
+
+// 展开我的左半边
+function showMyAll() {
+    $("#left_part").animate({
+        width: "show"
+    }, 200);
+
+    var mine = document.getElementById("mine");
+    var tips = mine.getElementsByClassName("tip_text");
+    $(tips[1]).show();
+    $(tips[0]).hide();
 }
