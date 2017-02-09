2d0c506340df2489d2d73cc8809cc181ce8b0a31
Jiayiwu <bestwujiayi@163.com>
2017-02-09 19:12:27 +0800

    Create the basis model

---
+++ server/src/main/java/com/vid/model/Relationship.java
@@ -0,0 +1,78 @@
+package com.vid.model;
+
+/**
+ * Created by Jiayiwu on 17/2/9.
+ * Mail:wujiayi@lgdreamer.com
+ * Change everywhere
+ */
+public class Relationship {
+    int id;
+//    关系拥有方
+    int ownerid;
+//    关系被拥有方(被名片扫码的那个人)
+    int friendid;
+//    被分组id
+    int groupid;
+//    视频id(冗余存储)
+    int videoid;
+//    视频名称(冗余存储)
+    String videoname;
+//    视频URL(冗余存储)
+    String videourl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerid() {
+        return ownerid;
+    }
+
+    public void setOwnerid(int ownerid) {
+        this.ownerid = ownerid;
+    }
+
+    public int getFriendid() {
+        return friendid;
+    }
+
+    public void setFriendid(int friendid) {
+        this.friendid = friendid;
+    }
+
+    public int getGroupid() {
+        return groupid;
+    }
+
+    public void setGroupid(int groupid) {
+        this.groupid = groupid;
+    }
+
+    public int getVideoid() {
+        return videoid;
+    }
+
+    public void setVideoid(int videoid) {
+        this.videoid = videoid;
+    }
+
+    public String getVideoname() {
+        return videoname;
+    }
+
+    public void setVideoname(String videoname) {
+        this.videoname = videoname;
+    }
+
+    public String getVideourl() {
+        return videourl;
+    }
+
+    public void setVideourl(String videourl) {
+        this.videourl = videourl;
+    }
+}
--- server/src/main/java/com/vid/model/User.java
+++ server/src/main/java/com/vid/model/User.java
@@ -5,10 +5,45 @@ package com.vid.model;
  * Mail:wujiayi@lgdreamer.com
  * Change everywhere
  */
-public class User {
 
+public class User {
+    private  int id;
+    //用户名
+    private String username;
+    //用户昵称
     private String name;
-    private Integer age;
+//    密码
+    private String password;
+//    用户头像保存位置
+    private String imgpath;
+//    注册时绑定的邮箱,如果用手机注册,则该字段为空.如用邮箱注册,则绑定邮箱和显示邮箱一致,但绑定邮箱不能更改,显示邮箱可以在个人设置中更改
+    private String bindingemail;
+//    名片对外展示的邮箱
+    private String showemail;
+//    同绑定邮箱规则
+    private String bindingtelephone;
+//    名片对外展示的电话
+    private String showtelephone;
+//    行业
+    private String industry;
+//    兴趣
+    private String interest;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public String getName() {
         return name;
@@ -18,21 +53,70 @@ public class User {
         this.name = name;
     }
 
-    public Integer getAge() {
-        return age;
+    public String getPassword() {
+        return password;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public User(String name, Integer age) {
-        super();
-        this.name = name;
-        this.age = age;
+    public String getImgpath() {
+        return imgpath;
+    }
+
+    public void setImgpath(String imgpath) {
+        this.imgpath = imgpath;
+    }
+
+    public String getBindingemail() {
+        return bindingemail;
+    }
+
+    public void setBindingemail(String bindingemail) {
+        this.bindingemail = bindingemail;
+    }
+
+    public String getShowemail() {
+        return showemail;
+    }
+
+    public void setShowemail(String showemail) {
+        this.showemail = showemail;
+    }
+
+    public String getBindingtelephone() {
+        return bindingtelephone;
+    }
+
+    public void setBindingtelephone(String bindingtelephone) {
+        this.bindingtelephone = bindingtelephone;
+    }
+
+    public String getShowtelephone() {
+        return showtelephone;
+    }
+
+    public void setShowtelephone(String showtelephone) {
+        this.showtelephone = showtelephone;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public void setInterest(String interest) {
+        this.interest = interest;
     }
 
     public User() {
-        super();
     }
 }
