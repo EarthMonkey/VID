bb3aa9db255d0ef0871ffd0b63c1b07a3888bc4c
SURE <794637366@qq.com>
2017-02-26 00:58:21 +0800

    好气哦

--- web/js/emailverify.js
+++ web/js/emailverify.js
@@ -2,6 +2,32 @@
  * Created by L.H.S on 2017/2/23.
  */
 
+function getParameter(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+
+function verify() {
+
+    var data = {
+        userID: parseInt(getParameter("userID")),
+        random: getParameter("random")
+    };
+
+    var xhr = sendXML(SERVER_IP + "/activate", "POST", data);
+    xhr.onreadystatechange = function () {
+        alert(xhr.status + "adf" + xhr.readyState)
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var result = xhr.response;
+
+            alert(result.status);
+        }
+    }
+
+}
+
 function gotoTime() {
     var span = $(".goto_btn").find("span");
     setInterval(function () {
