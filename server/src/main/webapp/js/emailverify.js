a77080bd4eab70edbbcae0b50fa68ad8dea93c30
SURE <794637366@qq.com>
2017-03-06 22:52:48 +0800

    重置密码

--- server/src/main/webapp/js/emailverify.js
+++ server/src/main/webapp/js/emailverify.js
@@ -9,24 +9,26 @@ function getParameter(name) {
     return null;
 }
 
-function verify() {
 
+/*
+ * 邮箱验证
+ * */
+function verify() {
 
     var data = "userID=" + getParameter("userID") + "&random=" + getParameter("random");
     var xhr = sendXML("/activate", "POST", data);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var result = xhr.response;
-           if (result.status == true) {
-               gotoTime();
-           }
+            if (result.status == true) {
+                gotoTime($(".goto_btn").find("span"));
+            }
         }
     }
-
 }
 
-function gotoTime() {
-    var span = $(".goto_btn").find("span");
+function gotoTime(span) {
+
     setInterval(function () {
         var second = span.html();
         if (second > 0) {
@@ -44,4 +46,70 @@ function gotoTime() {
 
 function gotoHomePage() {
     location.href = "Homepage.html";
+}
+
+
+/*
+ * 重置密码
+ * */
+function checkEmail() {
+
+    var data = "userID=" + getParameter("userID") + "&random=" + getParameter("random");
+    var xhr = sendXML("/verifyMail/findPass", "POST", data);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState==4 && xhr.status==200) {
+            var resp = xhr.response;
+            if (resp.status == true) {
+                $("#invalidDiv").hide();
+                $("#validDiv").show();
+            } else {
+                console.log(resp);
+            }
+        }
+    }
+
+}
+
+var ERR_LBL = $("<div class='err_lbl' style='line-height: 40px;'></div>");
+function resetPwd() {
+
+    var pwd = $(".modal_pwd").find("input")[0];
+    var pwd2 = $(".modal_pwd").find("input")[1];
+
+    if ($(pwd).val() == "") {
+        ERR_LBL.html("请输入密码");
+        $(pwd).parent("div").append(ERR_LBL);
+
+        $(pwd).focus(function () {
+            ERR_LBL.remove();
+        });
+
+        return;
+    }
+
+    if ($(pwd2).val() != $(pwd).val()) {
+        ERR_LBL.html("两次密码不一致");
+        $(pwd2).parent("div").append(ERR_LBL);
+
+        $(pwd2).focus(function () {
+            ERR_LBL.remove();
+        });
+
+        return;
+    }
+
+    var data = "password=" + pwd;
+    var xhr = sendXML("/resetPass", "POST", data);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var resp = xhr.response;
+            if (resp.status == true) {
+                $("#resetDiv").hide();
+                $("#successDiv").slideDown();
+                gotoTime($("#successDiv").find(".goto_btn").find("span"));
+            } else {
+                alert(resp.info);
+            }
+        }
+    };
 }
